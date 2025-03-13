c540e959f6d96b816dc74b10c867472e4ce1012e
Reuel Belizaire <Belizaire347@flagler.edu>
2025-03-13 14:56:46 -0400

    3/13/25

---
+++ Pages/FaQ.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlaglerBookSwap.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+            //test vvv
+        }
+    }
+}
